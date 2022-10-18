dd46026f0dec77a0442999a7eb81b56a29546efb
Oluwaseun Akinwale <oluwaseunazure@yahoo.com>
2022-10-18 23:34:53 +0100

    Add project files.

---
+++ MovieAPI/Data/ApiDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieAPI.Data
+{
+    public class ApiDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options)
+            : base(options)
+            { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
