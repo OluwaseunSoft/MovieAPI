e049069b9fe6dd7cc27df6e82f763440d37399cb
Oluwaseun Akinwale <oluwaseunazure@yahoo.com>
2022-10-23 20:19:31 +0100

    comment

--- MovieAPI/Controllers/CommentController.cs
+++ MovieAPI/Controllers/CommentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MovieAPI.Interfaces;
 using MovieAPI.Model;
@@ -6,6 +7,7 @@ using MovieAPI.Response_;
 
 namespace MovieAPI.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class CommentController : ControllerBase
